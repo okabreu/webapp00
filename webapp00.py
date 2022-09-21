a1d254c88a601e424c04b7473c1fe43bdeb13cdf
okabreu <109988700+okabreu@users.noreply.github.com>
2022-09-21 15:04:58 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("OS ROBACENA É FERA!")
+st.title("O ZERBINI É FERA!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Cabeçalho")
